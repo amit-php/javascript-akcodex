9eb99b69cdaea4e4535815445e426e5473bb3623
amit-php <59692221+amit-php@users.noreply.github.com>
2024-09-27 13:30:59 +0000

    for in for of done

---
+++ loops/arryloop.js
@@ -0,0 +1,10 @@
+const arr = [1,2,3,4,5,6];
+
+for (const element of arr) {
+    console.log(element);
+    
+}
+const str = "hello world";
+for (const element of str) {
+    console.log(`here is string ${element}`);
+}
